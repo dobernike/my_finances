a934541a91d18d4008630ffbdb67edea0c14351a
dobernike <Dobernike@mail.ru>
2020-06-23 23:24:51 +0300

    Update test

--- src/utils/__tests__/request.test.ts
+++ src/utils/__tests__/request.test.ts
@@ -1,33 +1,45 @@
+/* eslint-disable max-nested-callbacks */
 import { request } from '../request';
 
 import { depts } from '../../mocks/api/depts.mock.json';
 
 describe('utils', () => {
     describe('request', () => {
-        it('should return data if resolve', async () => {
-            // eslint-disable-next-line max-nested-callbacks
-            (global as any).fetch = jest.fn(() =>
-                Promise.resolve({
-                    // eslint-disable-next-line max-nested-callbacks
-                    json: () => Promise.resolve(depts),
-                })
-            );
+        describe('resolve', () => {
+            beforeAll(() => {
+                (global as any).fetch = jest.fn(() =>
+                    Promise.resolve({
+                        json: () => Promise.resolve(depts),
+                    })
+                );
+            });
 
-            const json = await request('/');
+            it('should return data with "GET"', async () => {
+                const json = await request('/');
 
-            expect(json).toEqual(depts);
+                expect(json).toEqual(depts);
+            });
+
+            it('should return data with "POST"', async () => {
+                const json = await request('/', 'POST', { data: 'data' });
+
+                expect(json).toEqual(depts);
+            });
         });
-
-        it('should throw error if reject', async () => {
+        describe('reject', () => {
             const error = { message: 'Some error is thrown' };
 
-            (global as any).fetch = jest.fn(() => Promise.reject(error));
+            beforeAll(() => {
+                (global as any).fetch = jest.fn(() => Promise.reject(error));
+            });
 
-            try {
-                await request('/');
-            } catch (e) {
-                expect(e.message).toEqual(error.message);
-            }
+            it('should throw error', async () => {
+                try {
+                    await request('/');
+                } catch (e) {
+                    expect(e.message).toEqual(error.message);
+                }
+            });
         });
     });
 });
